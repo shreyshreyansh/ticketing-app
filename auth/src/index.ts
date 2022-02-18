c471f969c384a350da8516ae55873faa161fb351
Shreyansh shrey <shreyansh.shrey1999@gmail.com>
2022-02-18 14:09:45 +0530

    add route handlers for user

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,19 @@
 import express from 'express';
 import { json } from 'body-parser';
 
+import { currentuserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+
 const app = express();
 app.use(json());
 
-app.get('/api/users/currentuser', (req, res) => {
-    res.send('Hello world!');
-})
+app.use(currentuserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
 
 app.listen('3000', () => {
     console.log(`\u001b[32mAuth listening on port 3000!\u001b[0m`);
