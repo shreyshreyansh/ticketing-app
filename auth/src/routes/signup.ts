bd8211e7259f00476e09ac2a8185fbc871f72ab5
Shreyansh shrey <shreyansh.shrey1999@gmail.com>
2022-02-21 13:35:14 +0530

    inital user creation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 
+import { User } from '../models/user';
+
 import { RequestValidationError } from '../errors/request-validation-error';
 import { DatabaseConnectionError } from '../errors/database-connection-error';
 
@@ -20,18 +22,42 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage('Password must be between 4 and 20 characters!'),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
+    /**
+     * STEPS:
+     * 1. Check the body send by the user using express-validator
+     * 2. Check if user exists with the given email address
+     * 3. Hash the password
+     * 4. Create a new user and store it in DB
+     * 5. Send the user a JWT token as a response
+     */
+
+    // STEP 1
     // we can find the result of the validation middleware by calling the validationResult function
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       // this will automatically be picked by the error handler middleware
       throw new RequestValidationError(errors.array());
     }
+
     const { email, password } = req.body;
 
-    // manually testing if the custom error works
-    throw new DatabaseConnectionError();
-    res.send({ msg: 'Hi there!' });
+    // STEP 2
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      console.log('Email in use');
+      return res.send({});
+    }
+
+    // STEP 4
+    const user = User.build({
+      email,
+      password,
+    });
+    await user.save();
+
+    // Simulating STEP 5
+    res.status(201).send(user);
   }
 );
 
