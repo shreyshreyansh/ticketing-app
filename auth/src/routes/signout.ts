c546c2443b8694ff11d3e0997b0cefc059b99b13
Shreyansh shrey <shreyansh.shrey1999@gmail.com>
2022-02-23 15:53:20 +0530

    add logout functionality on auth service

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,10 @@ import express from 'express';
 const router = express.Router();
 
 router.post('/api/users/signout', (req, res) => {
-  res.send('Hi there!');
+  // destroying a session
+  req.session = null;
+
+  res.status(200).send({});
 });
 
 export { router as signoutRouter };
